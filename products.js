8f72a0716a0c9a7af93b6f28dd33e72d086a8504
Emma Egstad <emma@egstad.com>
2021-10-11 14:15:46 -0700

    created object literal array

---
+++ products.js
@@ -0,0 +1,67 @@
+export const products = [
+    {
+        name: 'Wildflower Honey',
+        img: './assets/product-photos/wildflower-honey.jpeg',
+        description: '2oz jar of wildflower honey from Sauvie Island.',
+        category: 'Products',
+        price: 6,
+        id: '1',
+    },
+    {
+        name: 'Lavender Lip Balm',
+        img: './assets/product-photos/lavender-lip-balm.jpeg',
+        description:
+            'Handmade beeswax lip balm infused with lavender and chamomile.',
+        category: 'Products',
+        price: 2.5,
+        id: '2',
+    },
+    {
+        name: 'Peppermint Lip Balm',
+        img: './assets/product-photos/peppermint-lip-balm.jpeg',
+        description:
+            'Handmade beeswax lip balm infused with peppermint and calendula.',
+        category: 'Products',
+        price: 2.5,
+        id: '3',
+    },
+    {
+        name: 'Soothing Salve',
+        img: './assets/product-photos/peppermint-lip-balm.jpeg',
+        description:
+            'Handmade beeswax salve infused with St. Johns Wort, Plantain, and Chickweed.',
+        category: 'Products',
+        price: 5,
+        id: '4',
+    },
+    {
+        name: 'Honeybee Stickers',
+        img: './assets/product-photos/honeybee-stickers.jpeg',
+        description:
+            'A set a 3 original stickers with a worker bee, drone and queen.',
+        category: 'Products',
+        price: 4,
+        id: '5',
+    },
+    {
+        name: 'Solitary Bee Stickers',
+        img: './assets/product-photos/honeybee-stickers.jpeg',
+        description:
+            'A set a 3 original stickers with a leafcutter bee, mason bee and green metallic sweat bee.',
+        category: 'Products',
+        price: 4,
+        id: '6',
+    },
+];
+
+/* <h2 class="product-name">Wildflower Honey</h2>
+<img
+    src="./assets/Product Photos/Product-1.jpeg"
+    alt="A 2 ounce jar of Bee & Bloom wildflower honey."
+/>
+<p class="product-description">
+    2oz jar of wildflower honey from Sauvie Island.
+</p>
+<p class="product-category">Product</p>
+<p class="product-price">$5.00</p>
+<button class="product-add" value="1">Add to Cart</button> */
