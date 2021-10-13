82e7b82ee85855f8f70793ac833ad61cab5d04e9
Emma Egstad <emma@egstad.com>
2021-10-12 18:14:39 -0700

    fixed pathing in utils wow

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
-import { cart } from '../banana/cart-data.js';
-import { products } from '../banana/products.js';
+import { cart } from './banana/cart-data.js';
+import { products } from './banana/products.js';
 
 export function findById(id, array) {
     for (let item of array) {
